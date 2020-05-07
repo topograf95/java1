114b7ac69b0e9e428589945daeeb1335674fbf10
User1 <topograf95@yandex.ru>
2020-05-07 22:02:03 +0300

    Homework_8

--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -1,7 +1,8 @@
 package ru.progwards.java1.lessons.interfaces;
 
-enum CompareResult { LESS, EQUAL, GREATER }
+// enum CompareResult { LESS, EQUAL, GREATER }
 
 public interface CompareWeight {
+    public enum CompareResult { LESS, EQUAL, GREATER }
     public CompareResult compareWeight(CompareWeight smthHasWeigt);
 }
