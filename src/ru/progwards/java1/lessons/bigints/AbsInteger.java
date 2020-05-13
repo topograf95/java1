a16d204bef674c257d52b60e91de556e9ece60ba
User1 <topograf95@yandex.ru>
2020-05-13 03:08:41 +0300

    Homework_9

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -3,7 +3,10 @@ package ru.progwards.java1.lessons.bigints;
 public abstract class AbsInteger {
 
     static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        return null;
+        int sumNum = Integer.parseInt(num1.toString()) + Integer.parseInt(num2.toString());
+        if (sumNum > -129 && sumNum < 128) return new ByteInteger((byte)sumNum);
+        else if (sumNum > -32769 && sumNum < 32768) return new ShortInteger((short)sumNum);
+        else return new IntInteger(sumNum);
     }
 
     public abstract int add(int n1, int n2);
@@ -13,9 +16,10 @@ public abstract class AbsInteger {
     public abstract short add(short n1, short n2);
 
     public static void main(String[] args) {
-        ByteInteger by = new ByteInteger((byte) 5);
-        ShortInteger sh = new ShortInteger((short) 15);
-        IntInteger in = new IntInteger(25);
+        ByteInteger by = new ByteInteger((byte) 50);
+        ShortInteger sh = new ShortInteger((short) 1500);
+        IntInteger in = new IntInteger(250000);
         System.out.println(add(by, in));
+        System.out.println(add(sh, by));
     }
 }
